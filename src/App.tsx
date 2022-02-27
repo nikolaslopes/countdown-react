ce9dc33fe2781ba5e8d150c2520872028c7bc160
nikolaslopes <nikolaslopes.dev@gmail.com>
2022-02-26 21:58:15 -0400

    :fire: countdown finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
 import './App.css';
 
+const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 25 * 60; // 25 minutes
+
 function App() {
+  const [secondsAmount, setSecondsAmount] = useState(
+    COUNTDOWN_INITIAL_TIME_IN_SECONDS,
+  );
+
+  useEffect(() => {
+    if (secondsAmount === 0) {
+      toast('Time finished ⏲️', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+
+      return;
+    }
+
+    setTimeout(() => {
+      setSecondsAmount((state) => state - 1);
+    }, 1000);
+  }, [secondsAmount]);
+
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
   return (
     <main>
-      <h1>00</h1>
+      <h1>{String(minutes).padStart(2, '0')}</h1>
       <h1>:</h1>
-      <h1>00</h1>
+      <h1>{String(seconds).padEnd(2, '0')}</h1>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      theme="dark"
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </React.StrictMode>,
   document.getElementById('root')
 )
